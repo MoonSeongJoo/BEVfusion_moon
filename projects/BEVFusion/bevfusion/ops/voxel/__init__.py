12ac0d7eb8a92205440ed301c2eb7e2c6e9a59b2
YoungjuNa-KR <apercent1@naver.com>
2025-09-15 01:24:06 +0000

    modify for run our env

--- projects/BEVFusion/bevfusion/ops/voxel/__init__.py
+++ projects/BEVFusion/bevfusion/ops/voxel/__init__.py
@@ -1,4 +1,12 @@
 from .scatter_points import DynamicScatter, dynamic_scatter
 from .voxelize import Voxelization, voxelization
+# .so에 바인딩된 함수
+from .voxel_layer import dynamic_voxelize, hard_voxelize
 
-__all__ = ['Voxelization', 'voxelization', 'dynamic_scatter', 'DynamicScatter']
+# __all__ = ['Voxelization', 'voxelization', 'dynamic_scatter', 'DynamicScatter']
+
+__all__ = [
+    'Voxelization', 'voxelization', 
+    'dynamic_scatter', 'DynamicScatter',
+    'dynamic_voxelize', 'hard_voxelize'
+]
